00d8983dcd3d4ae8196cd751ef3bd84f589a6d9c
TimS <jshen44@fordham.edu>
2024-10-04 17:50:38 +0800

    ffn glu fixed

--- boring_nn/ffn/main.py
+++ boring_nn/ffn/main.py
@@ -12,14 +12,17 @@ from boring_utils.helpers import DEBUG
 
 
 class GLU(nn.Module):
-    def __init__(self, dim_in: int, dim_out: int, activation_type: ActivationType, use_bias: bool = True):
+    def __init__(self, dim_in: int, dim_out: int, config: FeedForwardConfig):
         super().__init__()
+        activation_type = config.activation.type
+        no_bias = config.no_bias
         self.act = BoringFeedForward.get_activation(activation_type)
-        self.proj = nn.Linear(dim_in, dim_out * 2, bias=use_bias)
+        self.proj = nn.Linear(dim_in, dim_out * 2, bias=not no_bias)
+        self.mult_bias = nn.Parameter(torch.ones(dim_out)) if config.activation.mult_bias else 1.
 
     def forward(self, x: Tensor) -> Tensor:
         x, gate = self.proj(x).chunk(2, dim=-1)
-        return x * self.act(gate)
+        return x * self.act(gate) * self.mult_bias
 
 
 class ReluSquared(nn.Module):
@@ -30,26 +33,24 @@ class ReluSquared(nn.Module):
 class BoringFeedForward(nn.Module):
     def __init__(self, config: FeedForwardConfig):
         super().__init__()
-        dim = config.d_model
-        dim_out = config.ffn_dim_out or dim
-        mult = config.mult
-        inner_dim = int(dim * mult)
+        dim_in = config.d_model
+        dim_out = config.ffn_dim_out or dim_in
+        mult_dim = config.mult_dim
+        inner_dim = int(dim_in * mult_dim)
 
         activation_type = config.activation.type
         use_glu = config.activation.use_glu
 
         # project_in is the first layer of the FFN
         if use_glu:
-            self.glu = GLU(dim, inner_dim, activation_type, use_bias=not config.no_bias)
-            project_in = self.glu
+            project_in = GLU(dim_in, inner_dim, config)
         else:
-            activation = self.get_activation(activation_type)
             project_in = nn.Sequential(
-                nn.Linear(dim, inner_dim, bias=not config.no_bias),
-                activation
+                nn.Linear(dim_in, inner_dim, bias=not config.no_bias),
+                self.get_activation(activation_type)
             )
 
-        self.net = nn.Sequential(
+        self.feedforward = nn.Sequential(
             project_in,
             nn.LayerNorm(inner_dim) if config.post_act_ln else nn.Identity(),
             nn.Dropout(config.dropout),
@@ -57,7 +58,7 @@ class BoringFeedForward(nn.Module):
         )
 
         if config.zero_init_output:
-            nn.init.zeros_(self.net[-1].weight)
+            nn.init.zeros_(self.feedforward[-1].weight)
 
         self.config = config
 
@@ -76,4 +77,4 @@ class BoringFeedForward(nn.Module):
             raise ValueError(f"Unsupported activation type: {activation_type}")
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.net(x)
+        return self.feedforward(x)
